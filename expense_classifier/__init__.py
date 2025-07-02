03b0b489e6a12ddeed6dc5572d894764b5ffc406
Petrinax <piyushupreti07@gmail.com>
2025-07-03 01:12:08 +0530

    Category Addition - Category Addition - Readme Update - Dynamic versioning - package setup updates

--- expense_classifier/__init__.py
+++ expense_classifier/__init__.py
@@ -1,6 +1,6 @@
 """Expense Classifier - A tool for categorizing bank transactions."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from expense_classifier.pipeline import Pipeline
 from expense_classifier.transformer import Transformer
